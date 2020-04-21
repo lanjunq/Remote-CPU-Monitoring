c8534386c6b511fa7e35d69bf350498dc5a0229a
lanjun <lanjunq8@gmail.com>
2020-04-21 18:49:20 -0400

    Beautify interface.html and webpage.js source code by adding comments

--- cpu_monitoring.c
+++ cpu_monitoring.c
@@ -7,36 +7,36 @@ int cpu_idle_time();
 // int main(){
 
 // 	cpu_idle_time();
-	
+
 // 	return 0;
 // }
 
 /* A helper function that returns CPU idle time being whenever called */
 int cpu_idle_time(){
-	
+
 	// Declare variables
 	int idle_time;
 	char buffer [100];
-	
-	int target = 4; 
+
+	int target = 4;
 	char* start;
-	
+
 	// Read a line file
 	FILE* fp = fopen("/proc/stat", "r");
 	fread(buffer, sizeof(char), 100, fp);
 // 	printf("line: %s \n", buffer);
-	
+
 	// Extract idle time
 	start = strtok(buffer, " ");
 	for (int i = 0; i < target; i++){
 		start = strtok(NULL, " ");
 	}
-	
+
 	// Convert idle time to int
 	idle_time = atoi(start);
-	
+
 	fclose(fp);
-	
+
 	return idle_time;
 }
 
@@ -45,11 +45,11 @@ extern int idle_history [];
 extern int count;
 void update_cpu_statistics(){
 	// Objective: use idle_history to update usage_*
-	
+
 	// Read and analyze
 	int latest = idle_history[count]; // Note: not yet updated count when executing this function, i.e. idle_history[count] is the lastest
 	int previous = idle_history[count - 1];
-	
+
 	// Compute new CPU status
 	double idle_time = (latest - previous) / 4; // idle time in %
 	double usage = 100 - idle_time; // CPU work time in %
@@ -58,7 +58,7 @@ void update_cpu_statistics(){
 	printf("raw usage \% \t: %f \n", usage);
 	if (usage < 0) usage = 0;
 	if (usage > 100) usage = 100;
-	
+
 	// Update CPU status
 	usage_latest = usage;
 	usage_avg = (usage_avg * count + usage) / (count + 1);
--- webpage.js
+++ webpage.js
@@ -1,117 +1,112 @@
 /* --------------------------------------------------------------- */
 /* -------------------------- Variables -------------------------- */
-var usage_max = undefined, usage_avg = undefined, usage_latest = undefined;
+// CPU usage variables
+var usage_max = undefined;
+var usage_avg = undefined;
+var usage_latest = undefined;
 
+// webpage property variables
+var auto_update = false;            // auto-update is on(true) or off(false)
+var threshold = undefined;          // threshold for cpu usage alarm
+var frequency = 1;                  // auto-update frequency (second)
+var auto_update_handle = undefined; // handle to stop auto-update
+
+// visual configuration variables
 var config = {
-  width : 300,
-  height : 400,
-  background_color : 'gray',
-  bar_color : 'green',
-  xAxis_len : 200,
-  yAxis_len : 300,
-  x_offset : 50,
-  y_offset : 320
+  width : 300,                      // svg width
+  height : 400,                     // svg height
+  background_color : 'gray',        //
+  bar_color : 'green',              //
+  xAxis_len : 200,                  // length of x-axis
+  yAxis_len : 300,                  // length of y-aixs
+  x_offset : 50,                    // offset from left-boudnary of svg
+  y_offset : 320                    // offset from top-boudnary of svg
 };
 
 
-var auto_update = false;
-var threshold = undefined;
-var frequency = 1; // communication frequency and display interval
-var auto_update_handle;
-
 /* --------------------------------------------------------------- */
 /* ------------------------- Setup Chart ------------------------- */
+
+// select svg and set width and height
 var svg = d3.select('svg')
-// svg.selectAll('rect')
   .attr('width', `${config.width}`)
   .attr('height', `${config.height}`);
-//   .attr('style', 'fill:gray');
 
-// x-axis
+// Setup x-axis
 var xScale = d3.scaleLinear()
-  .domain([0, 1]) // value
-  .range([0, config.xAxis_len]);
+  .domain([0, 1])                 // x-axis value
+  .range([0, config.xAxis_len]);  // x-axis length
 var xAxis = d3.axisBottom(xScale);
-
+// Draw x-axis
 svg.append('g')
-  .attr('transform', `translate(${config.x_offset}, ${config.y_offset})`)
+  .attr('transform', `translate(${config.x_offset}, ${config.y_offset})`) // calculate x-axis location
   .call(xAxis);
 
-// y-axis
+// Setup y-axis
 var yScale = d3.scaleLinear()
-  .domain([100, 0]) // value
-  .range([0, config.yAxis_len]);
+  .domain([100, 0])               // y-axis value
+  .range([0, config.yAxis_len]);  // y-axis length
 var yAxis = d3.axisLeft(yScale);
-
+// Draw y-axis
 svg.append('g')
-  .attr('transform', `translate(${config.x_offset}, ${config.y_offset-config.yAxis_len})`)
+  .attr('transform', `translate(${config.x_offset}, ${config.y_offset-config.yAxis_len})`) // calculate y-axis location
   .call(yAxis);
 
-// empty bar
+// Initialize bar display
 svg.append('g').append('rect')
-  .attr('transform', `translate(${config.x_offset}, ${(1-usage_latest/100) * config.yAxis_len})`)
+  .attr('transform', `translate(${config.x_offset}, ${(1-usage_latest/100) * config.yAxis_len})`) // Initialize the bar display
   .attr('width', `${config.xAxis_len}`)
-  // .attr('width', '140')
   .attr('height', '0');
-  // .attr('height', '40');
-
-
-
-/* -------------------------------------------------------------- */
-/* ---------------------------- test ---------------------------- */
-
 
 
 /* -------------------------------------------------------------- */
 /* --------------------- Callback functions --------------------- */
-/* Helper function to automatically execute the update operation */
 
+/* Helper function to automatically execute the update operation */
 function enable_auto_update () {
-  console.log('Start Auto-update');
   auto_update = true;
-  auto_update_handle = setInterval(update, frequency * 1000);
-  $('#auto_update_status').removeClass('off');
+  auto_update_handle = setInterval(update, frequency * 1000); // enable auto-update
+  $('#auto_update_status').removeClass('off');  // visual changes to remind user
   $('#auto_update_status').addClass('on');
   $('#auto_update_status').val('On');
 }
 
-function refresh_update_frequency(){
-  if (auto_update){
-    // QUESTION: Do I need to clear the previous setInterval?
-    clearInterval(auto_update_handle);
-    auto_update_handle = setInterval(update, frequency * 1000);
-  }
-}
-
+/* Helper function to stop auto-update (not yet used) */
 function disable_auto_update(){
   clearInterval(auto_update_handle);
   auto_update = false;
-  $('#auto_update_status').addClass('off');
-  $('#auto_update_status').removeClass('on');
-  $('#auto_update_status').val('Off');
+  $('#auto_update_status').addClass('off');   //
+  $('#auto_update_status').removeClass('on'); //
+  $('#auto_update_status').val('Off');        //
 }
 
 function set_threshold(){
-  if (threshold){ // Disable alarm
+  // Disable alarm
+  if (threshold){                             // if the threshold is truthy value (not undefined)
     threshold = undefined;
-    $('#threshold_input').removeClass('on');
-  } else { // Enable alarm
-    threshold = $('#threshold_input').val();
-    $('#threshold_input').addClass('on');
+    $('#threshold_input').removeClass('on');  // remove back-ground color green
+  }
+  // Enable alarm
+  else {
+    threshold = $('#threshold_input').val();  // read threshold value from box
+    $('#threshold_input').addClass('on');     // change background-color to green
   }
 }
 
+/* Callback function to change the page 'refresh requency' */
 function change_frequency(){
-  frequency = $('#frequency_input').val(); // frequency as in text
-  frequency = parseInt(frequency, 10); // frequency as in number
-  $('#frequency_input').addClass('on');
-  refresh_update_frequency();
-  // update();
+  frequency = $('#frequency_input').val();    // frequency as in text
+  frequency = parseInt(frequency, 10);        // frequency as in number
+  $('#frequency_input').addClass('on');       // change background-color to green
+  refresh_update_frequency();                 // call helper function
 }
 
 
 /* -------------------------------------------------------------- */
 /* ---------------------- Helper functions ---------------------- */
+
+/* Helper function for auto-update. update() is called each time the
+  webpage needs to be 'refreshed' */
 function update(){
   communication();
   update_numbers();
@@ -119,27 +114,32 @@ function update(){
   threshold_alarm();
 }
 
+/* Helper function to udpate the CPU statistics section on the webpage */
 function update_numbers(){
   $('#usage_max').val(usage_max);
   $('#usage_avg').val(usage_avg);
   $('#usage_latest').val(usage_latest);
 }
 
+/* Helper function to update the chart display section on the webpage */
 function update_chart(){
   $('rect')
     .attr('transform', `translate(${config.x_offset}, ${config.y_offset-usage_latest/100 * config.yAxis_len})`)
     .attr('height', `${usage_latest/100 * config.yAxis_len}`);
 }
 
+/* Helper function to send HTTP request to the server
+  and update the CPU statistics */
 function communication(){
-  $.getJSON("/data", function (data, status) {
-    count = data.count;
+  $.getJSON("/data", function (data, status) {    // send HTTP request
+    count = data.count;                           // update the CPU statistics
     usage_max = data.usage_max;
     usage_avg = data.usage_avg;
     usage_latest = data.usage_latest;
   })
 }
 
+/* Helper function to alarm CPU usage pass threshold */
 function threshold_alarm(){
   if (threshold && usage_latest > threshold){
     alert("Latest CPU usage exceeds the threshold: " + threshold);
@@ -147,9 +147,18 @@ function threshold_alarm(){
   }
 }
 
+/* Helper function to update auto-update frequency when user inputs a new frequency */
+function refresh_update_frequency(){
+  if (auto_update){
+    clearInterval(auto_update_handle);
+    auto_update_handle = setInterval(update, frequency * 1000);
+  }
+}
+
 
 /* -------------------------------------------------------------- */
 /* --------------------------- jQuery --------------------------- */
+// Associate buttons to corresponding callback functions
 update();
 $('#auto_update_button').click(enable_auto_update);
 $('#threshold_button').click(set_threshold);
